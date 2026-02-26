cc069ad8bfef89486c25ef16ad6d655e0bc332fd
randomsena0101 <166689915+randomsena0101@users.noreply.github.com>
2026-02-26 12:41:30 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -23,7 +23,7 @@ from decouple import config
 API_ID = config("API_ID", cast=int)           # 36905571
 API_HASH = config("API_HASH")                 # 36677bbab05f148b95f91b13dbc57ea1
 BOT_TOKEN = config("BOT_TOKEN")               # 8737801641:AAE7QoFn2deJt2WfTjVDt10hJGQow0wwgHA
-SESSION = config("SESSION")                   # your Telethon session string
+SESSION = config("SESSION")                   # AQIzImMAI_Bzq8pvW5EtPvSvTfAfd4o7AFQd4CxBLf_KqDZMlLbcmHMPreQBjSh32nW6GctDwJcOsLBIYzgPw0P9RjOUv0wGUoOqfqxHASEV3sH_9xpez-J-TaDisZc8ILUwCXhfHOmES13sVPJA-D_rOkRolgZB5t0v-gmlD-54FfDqqYjHZZGy4plUGxVyEZrg2y3xhjWbAqTdQhvXKCjHUZ6Sy_MxmCtJ2SYeNmdjP3wpvgiU43Fog7AJ_dSGVWcjtaTs9eR-5G_R7N8WqoNxziamBEdgkxrZyizFt0tMZU3PJiMSgi8x3u1tEyX45lApMNcfitXorxGZtrOY0L5bsIKM-wAAAAHpC8m5AA
 FORCESUB = config("FORCESUB", default="")    # aksjuatest
 AUTH = config("AUTH", cast=int, default=0)   # 8204831161
 # Validate critical credentials
